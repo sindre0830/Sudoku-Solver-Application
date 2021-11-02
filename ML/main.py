585070ac2851957e20e0a1fc0e78c621fccd3765
sindre0830 <sindre0830@gmail.com>
2021-11-02 18:29:06 +0100

    [#17] Enable memory growth for gpu devices

--- ML/main.py
+++ ML/main.py
@@ -8,6 +8,16 @@ from model import (
     generateModel,
     trainModel
 )
+# import foreign modules
+import tensorflow as tf
+
+
+# enable memory growth for gpu devices
+# source: https://stackoverflow.com/a/55541385/8849692
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+if gpu_devices:
+    for devices in gpu_devices:
+        tf.config.experimental.set_memory_growth(devices, True)
 
 
 # download MNIST dataset and perform preprocessing
