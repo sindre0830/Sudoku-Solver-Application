7880778b966632c22cdd9e7f9d38ab0292e56a92
sindre0830 <sindre0830@gmail.com>
2021-11-02 17:39:15 +0100

    [#5] Add reshapeDataset()

--- ML/main.py
+++ ML/main.py
@@ -1,7 +1,9 @@
 from preprocessing import (
-    downloadDataset
+    downloadDataset,
+    reshapeDataset
 )
 
 
 # download MNIST dataset
 xTrain, xTest, yTrain, yTest = downloadDataset()
+xTrain, xTest, yTrain, yTest = reshapeDataset(xTrain, xTest, yTrain, yTest)
--- ML/preprocessing.py
+++ ML/preprocessing.py
@@ -1,7 +1,17 @@
 from tensorflow.keras.datasets import mnist
+import keras.utils.np_utils
 
 
 # Download MNIST dataset.
 def downloadDataset():
     (xTrain, yTrain), (xTest, yTest) = mnist.load_data()
+    return xTrain, xTest, yTrain, yTest
+
+
+# Reshape data to four dimensions and perform categorical on labels.
+def reshapeDataset(xTrain, xTest, yTrain, yTest):
+    xTrain = xTrain.reshape(xTrain.shape[0], xTrain.shape[1], xTrain.shape[2], 1).astype('float32')
+    xTest = xTest.reshape(xTest.shape[0], xTest.shape[1], xTest.shape[2], 1).astype('float32')
+    yTrain = keras.utils.np_utils.to_categorical(yTrain)
+    yTest = keras.utils.np_utils.to_categorical(yTest)
     return xTrain, xTest, yTrain, yTest
