9e7fe623b0b3342a61067f08c9d5a3dee5c46f09
Aksel Skaar Leirvaag <akselsle@mobai.bio>
2021-11-03 14:39:06 +0100

    [#6] feat: load image from gallery

--- Application/app/src/main/java/com/example/sudokusolver/ImageLoadingActivity.kt
+++ Application/app/src/main/java/com/example/sudokusolver/ImageLoadingActivity.kt
@@ -1,51 +1,36 @@
 package com.example.sudokusolver
 
-import android.graphics.drawable.Icon
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Camera
 import androidx.compose.material.icons.rounded.Casino
 import androidx.compose.material.icons.rounded.Collections
 import androidx.compose.material.icons.rounded.PhotoCamera
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.sudokusolver.ui.theme.SudokuSolverTheme
 
 class ImageLoadingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        data class ActivityButton(
-            val icon: ImageVector,
-            val descriptionResourceId: Int,
-            val onClick: () -> Unit
-        )
-
-        val buttons = listOf(
-            ActivityButton(
-                Icons.Rounded.PhotoCamera,
-                R.string.image_loading_activity_icon_description_camera,
-                {/*TODO: Launch Camera*/ }
-            ),
-            ActivityButton(
-                Icons.Rounded.Collections,
-                R.string.image_loading_activity_icon_description_gallery,
-                { /*TODO: Launch Gallery*/ },
-            ),
-            ActivityButton(
-                Icons.Rounded.Casino,
-                R.string.image_loading_activity_icon_description_random,
-                { /*TODO: Load hardcoded/random board*/ }
-            )
-
-        )
-
         super.onCreate(savedInstanceState)
         setContent {
             SudokuSolverTheme {
@@ -63,29 +48,93 @@ class ImageLoadingActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.Center,
                         modifier = Modifier.fillMaxSize()
                     ) {
-                        buttons.forEach {
-                            Button(
-                                onClick = it.onClick,
-                                modifier = Modifier.size(width = 200.dp, height = 60.dp)
-                            ) {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    modifier = Modifier.width(200.dp)
-                                ) {
-                                    Icon(
-                                        it.icon,
-                                        contentDescription = stringResource(id = it.descriptionResourceId),
-                                        modifier = Modifier.size(32.dp)
-                                    )
-                                    Spacer(modifier = Modifier.padding(10.dp))
-                                    Text(text = stringResource(id = it.descriptionResourceId))
-                                }
-                            }
-                            Spacer(modifier = Modifier.padding(20.dp))
-                        }
+                        ButtonWithIcon(
+                            onClick = {/*TODO: Launch Camera*/ },
+                            icon = Icons.Rounded.PhotoCamera,
+                            descriptionResourceId = R.string.image_loading_activity_icon_description_camera,
+                        )
+                        LoadImageFromGalleryBtn()
+                        ButtonWithIcon(
+                            onClick = { /*TODO: Load hardcoded/random board*/ },
+                            icon = Icons.Rounded.Casino,
+                            descriptionResourceId = R.string.image_loading_activity_icon_description_random,
+                        )
                     }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ButtonWithIcon(
+    onClick: () -> Unit,
+    icon: ImageVector,
+    descriptionResourceId: Int,
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier.size(width = 200.dp, height = 60.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(200.dp)
+        ) {
+            Icon(
+                icon,
+                contentDescription = stringResource(id = descriptionResourceId),
+                modifier = Modifier.size(32.dp)
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+            Text(text = stringResource(id = descriptionResourceId))
+        }
+    }
+    Spacer(modifier = Modifier.padding(20.dp))
+}
+
+@Composable
+fun LoadImageFromGalleryBtn() {
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
+    val context = LocalContext.current
+    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+    val launcher = rememberLauncherForActivityResult(
+        contract =
+        ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        imageUri = uri
+    }
+
+    ButtonWithIcon(
+        onClick = { launcher.launch("image/*") },
+        icon = Icons.Rounded.Collections,
+        descriptionResourceId = R.string.image_loading_activity_icon_description_gallery,
+    )
+    imageUri?.let { uri ->
+        bitmap.value = imageUriToBitmap(context, uri)
+        bitmap.value?.let { btm ->
+            // TODO: Remove this image. This image should be processed to an array of ints.
+            Image(
+                bitmap = btm.asImageBitmap(),
+                contentDescription = "image loaded",
+                modifier = Modifier.size(400.dp)
+            )
+        }
+    }
+
+}
+
+fun imageUriToBitmap(context: Context, uri: Uri): Bitmap {
+    return if (Build.VERSION.SDK_INT < 28) {
+        MediaStore.Images
+            .Media.getBitmap(context.contentResolver, uri)
+
+    } else {
+        val source = ImageDecoder
+            .createSource(context.contentResolver, uri)
+        ImageDecoder.decodeBitmap(source)
     }
 }
