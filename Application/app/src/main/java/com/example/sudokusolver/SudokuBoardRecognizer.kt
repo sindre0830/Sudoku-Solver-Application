cc746070122251e55ed9790adf774800f110789e
sindre0830 <sindre0830@gmail.com>
2021-11-07 16:48:46 +0100

    [#20] Add loadOpenCV() and execute()

--- Application/app/src/main/java/com/example/sudokusolver/SudokuBoardRecognizer.kt
+++ Application/app/src/main/java/com/example/sudokusolver/SudokuBoardRecognizer.kt
@@ -1,4 +1,20 @@
 package com.example.sudokusolver
 
+import android.util.Log
+import org.opencv.android.OpenCVLoader
+
 class SudokuBoardRecognizer {
+    private var dependenciesLoaded = loadOpenCV()
+
+    fun execute() {
+        //check if dependencies has been loaded
+        if (!dependenciesLoaded) {
+            Log.e("OpenCV", "OpenCV failed to load and is preventing image recognition")
+            return
+        }
+    }
+
+    private fun loadOpenCV(): Boolean {
+        return OpenCVLoader.initDebug()
+    }
 }
