b90dbded77e2b1f15607f1707f2954d835dcfb6f
sindre0830 <sindre0830@gmail.com>
2021-11-07 17:03:48 +0100

    [#20] Add convertToGrayscale()

--- Application/app/src/main/java/com/example/sudokusolver/SudokuBoardRecognizer.kt
+++ Application/app/src/main/java/com/example/sudokusolver/SudokuBoardRecognizer.kt
@@ -6,7 +6,9 @@ import android.util.Log
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.Mat
+import org.opencv.core.Size
 import org.opencv.imgcodecs.Imgcodecs
+import org.opencv.imgproc.Imgproc
 
 class SudokuBoardRecognizer constructor(private val context: Context) {
     private var dependenciesLoaded = loadOpenCV()
@@ -18,14 +20,28 @@ class SudokuBoardRecognizer constructor(private val context: Context) {
             Log.e("OpenCV", "OpenCV failed to load and is preventing image recognition")
             return
         }
+        //perform preprocessing
+        extractBoard()
     }
 
     private fun loadOpenCV(): Boolean {
         return OpenCVLoader.initDebug()
     }
 
+    private fun extractBoard() {
+        val fullImage = getOriginalImage()
+        //perform preprocessing of image
+        convertToGrayscale(fullImage)
+    }
+
     private fun getOriginalImage(): Mat {
         return this.originalImage.clone()
+    }
+
+    private fun convertToGrayscale(matrix: Mat) {
+        val bufferMatrix = generateBuffer(matrix)
+        Imgproc.cvtColor(bufferMatrix, matrix, Imgproc.COLOR_BGR2GRAY)
+        bufferMatrix.release()
     }
 
     private fun generateBuffer(matrix: Mat): Mat {
