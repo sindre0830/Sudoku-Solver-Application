14d4bb278865c455076abb41d477cb2e0c7b2ffa
rickarl <rickarl@stud.ntnu.no>
2021-11-08 14:51:53 +0100

    [#2] Some refactoring, remove unnecessary code.

--- Application/app/src/main/java/com/example/sudokusolver/Level.kt
+++
@@ -1,9 +0,0 @@
-// Source: https://github.com/TypicalDevStuff/sudoku-generator
-
-package com.example.sudokusolver
-
-enum class Level(val numProvidedDigits: Int) {
-    BEGINNER(25),
-    MEDIUM(20),
-    ADVANCED(17);
-}
