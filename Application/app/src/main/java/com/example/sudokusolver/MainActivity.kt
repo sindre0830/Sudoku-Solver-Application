d7ff23f0f7bbdf7740afd8f24e2f5024b02eb3f0
Aksel Skaar Leirvaag <akselsle@mobai.bio>
2021-11-10 11:44:48 +0100

    [#13] feat: store board persistently after solved

--- Application/app/src/main/java/com/example/sudokusolver/MainActivity.kt
+++ Application/app/src/main/java/com/example/sudokusolver/MainActivity.kt
@@ -3,9 +3,9 @@ package com.example.sudokusolver
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -19,25 +19,35 @@ import androidx.compose.ui.unit.dp
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.lifecycle.lifecycleScope
 import com.example.sudokusolver.ui.theme.ColorBoxSelected
 import com.example.sudokusolver.ui.theme.SudokuSolverTheme
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 const val SUDOKU_BOARD_KEY = "sudoku_board"
+const val SUDOKU_BOARD_HISTORY_KEY = "sudoku_board_history"
+
+// adding store here to ensure it is a singleton
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = SUDOKU_BOARD_KEY)
+
 typealias mutateBoardFn = (index: Int, backgroundColor: Color, number: Int) -> Unit
 typealias mutateBoardColorFn = (index: Int, backgroundColor: Color) -> Unit
 typealias mutateBoardNumberFn = (index: Int, number: Int) -> Unit
 
 class MainActivity : ComponentActivity() {
     private val SUDOKU_BOARD = stringPreferencesKey(SUDOKU_BOARD_KEY)
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = SUDOKU_BOARD_KEY)
+    private val SUDOKU_BOARD_HISTORY = stringPreferencesKey(SUDOKU_BOARD_HISTORY_KEY)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +63,9 @@ class MainActivity : ComponentActivity() {
             val sudokuBoard: SnapshotStateList<SudokuBoardItem> =
                 remember { setupBoard(board) }
             val (sudokuBoxClicked, setSudokuBoxClicked) = remember { mutableStateOf(0) }
-            val history: SnapshotStateList<HistoryItem> = remember { mutableStateListOf() }
+            val currentGameHistory: SnapshotStateList<CurrentGameHistoryItem> =
+                remember { mutableStateListOf() }
+
             fun isNewNumber(old: Int, new: Int) = old != new
 
             // mutateBoard mutates a sudoku item and allows for optional arguments
@@ -67,8 +79,8 @@ class MainActivity : ComponentActivity() {
 
                 number?.let {
                     if (isNewNumber(sudokuBoard[index].number, number)) {
-                        history.add(
-                            HistoryItem(
+                        currentGameHistory.add(
+                            CurrentGameHistoryItem(
                                 newValue = number,
                                 oldValue = sudokuBoard[index].number,
                                 sudokuItem = index
@@ -104,7 +116,21 @@ class MainActivity : ComponentActivity() {
                         verticalArrangement = Arrangement.SpaceBetween,
                     ) {
                         Header {
-                            HistoryIcon()
+                            HistoryIcon(
+                                modifier = Modifier.clickable {
+                                    Intent(
+                                        applicationContext,
+                                        HistoryActivity::class.java
+                                    ).apply {
+                                        putExtra(
+                                            SUDOKU_BOARD_HISTORY_KEY,
+                                            ArrayList(runBlocking { loadBoardHistory() })
+                                        )
+                                    }.also {
+                                        startActivity(it)
+                                    }
+                                }
+                            )
                         }
                         SudokuBoard(
                             items = sudokuBoard,
@@ -126,8 +152,8 @@ class MainActivity : ComponentActivity() {
                         ActionMenu(
                             handleActionMenuItems(
                                 sudokuItemClicked = sudokuBoxClicked,
-                                boardSize = sudokuBoard.size,
-                                history = history,
+                                board = sudokuBoard,
+                                currentGameHistory = currentGameHistory,
                                 startImageLoadingActivity = {
                                     Intent(
                                         applicationContext,
@@ -138,6 +164,9 @@ class MainActivity : ComponentActivity() {
                                 },
                                 mutateBoard = { i, bg, num -> mutateBoard(i, bg, num) },
                                 mutateBoardNumber = { i, num -> mutateBoardNumber(i, num) },
+                                addSudokuBoardAsSolved = { solvedBoardNumbers ->
+                                    lifecycleScope.launch { saveBoardToHistory(solvedBoardNumbers) }
+                                }
                             )
                         )
                         BottomNumbers(handleClick = { numberClicked ->
@@ -162,6 +191,31 @@ class MainActivity : ComponentActivity() {
     private suspend fun loadBoard(): List<Int>? {
         val pref = dataStore.data.first()
         return pref[SUDOKU_BOARD]?.split(",")?.map { it.toInt() }
+    }
+
+    private suspend fun saveBoardToHistory(board: List<Int>) {
+        dataStore.edit { pref ->
+            val historyEntries = loadBoardHistory()
+            historyEntries.add(
+                PreviousGamesHistoryItems(
+                    SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date()),
+                    board
+                )
+            )
+            pref[SUDOKU_BOARD_HISTORY] = Gson().toJson(historyEntries)
+        }
+    }
+
+    private suspend fun loadBoardHistory(): MutableList<PreviousGamesHistoryItems> {
+        val pref = dataStore.data.first()
+        val historyStr = pref[SUDOKU_BOARD_HISTORY] ?: return mutableListOf()
+        val gson = Gson()
+        val historyEntriesType =
+            object : TypeToken<MutableList<PreviousGamesHistoryItems>>() {}.type
+        return gson.fromJson(
+            historyStr,
+            historyEntriesType
+        )
     }
 }
 
