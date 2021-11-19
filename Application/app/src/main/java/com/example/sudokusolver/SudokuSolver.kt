3df0bbb88a25563a1aed249deb0cd48ba75569e8
rickarl <rickarl@stud.ntnu.no>
2021-11-19 15:49:12 +0100

    [#66] Added function to check if a filled board is legal

--- Application/app/src/main/java/com/example/sudokusolver/SudokuSolver.kt
+++ Application/app/src/main/java/com/example/sudokusolver/SudokuSolver.kt
@@ -190,4 +190,18 @@ class SudokuSolver constructor(private val context: Context) {
         }
         return newgrid
     }
+
+    // Checks that each box sees every number in row, column and square
+    private fun checkValid(board: Array<Array<Int>>): Boolean {
+        for (i in (0 until rows)) {
+            for (j in (0 until columns)) {
+                if (removeRow(board[i][j], board).isNotEmpty() ||
+                    removeCol((1..9).toMutableList(), board[i][j], board).isNotEmpty() ||
+                    removeSquare((1..9).toMutableList(), board[i][j], board).isNotEmpty()) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 }
